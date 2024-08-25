027a3b87849a4d46e23ff52d5eb3bbe9589121cf
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-25 13:54:50 -0700

    feat: Add database package

---
+++ packages/database/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
