bc60793edbe4641861d580db194b0ec5c1d78f8a
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-26 11:26:42 -0700

    feat: Add next-auth

---
+++ apps/web/auth/index.ts
@@ -0,0 +1,49 @@
+import NextAuth, { NextAuthResult } from "next-auth";
+//import CredentialsProvider from "next-auth/providers/credentials";
+import Nodemailer from "next-auth/providers/nodemailer";
+//import { compare } from "bcrypt";
+import authConfig from "./config";
+
+const nextauth = NextAuth({
+    ...authConfig,
+    providers: [
+        /*
+        CredentialsProvider({
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                const { email, password } = credentials ?? {}
+                if (!email || !password) {
+                    throw new Error("Missing email or password");
+                }
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: email as string,
+                    },
+                });
+                // if user doesn't exist or password doesn't match
+                if (!user || !user.password || !(await compare(password as string, user.password))) {
+                    throw new Error("Invalid email or password")
+                }
+                return user;
+            },
+        }),
+        */
+        Nodemailer({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: parseInt(process.env.EMAIL_SERVER_PORT as string),
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD
+                }
+            },
+            from: process.env.EMAIL_FROM
+        })
+    ]
+});
+
+export const auth: NextAuthResult["auth"] = nextauth.auth;
+export const handlers: NextAuthResult["handlers"] = nextauth.handlers;
