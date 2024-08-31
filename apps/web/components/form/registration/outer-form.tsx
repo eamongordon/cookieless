3aa3a1184d934b45567a47af3838a75d14377d2a
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-30 17:49:08 -0700

    refactor: render only login or signup form on page

--- apps/web/components/form/registration/outer-form.tsx
+++ apps/web/components/form/registration/outer-form.tsx
@@ -33,110 +33,114 @@ export default function FormWrapper(
           //fullWidth
           //size="md"
           aria-label="Shift between Login and Signup forms"
-          //selectedKey={isModal ? selected.toString() : pathname}
-          //onSelectionChange={setSelected}
-          /*classNames={{
-            tabList: "rounded-t-xl rounded-b-none",
-            tab: "rounded-t-xl rounded-b-none",
-          }}*/
+        //selectedKey={isModal ? selected.toString() : pathname}
+        //onSelectionChange={setSelected}
+        /*classNames={{
+          tabList: "rounded-t-xl rounded-b-none",
+          tab: "rounded-t-xl rounded-b-none",
+        }}*/
         >
-          <div key="/login" title="Log In" {...(isModal ? {} : { href: "/login", as: Link })}>
-            <Image
-              alt="Prospector Minerals"
-              width={100}
-              height={100}
-              className="relative mx-auto h-12 w-auto dark:scale-120 dark:rounded-full dark:border dark:border-stone-400 my-5"
-              src="/PM-Favicon-New-Square.svg"
-            />
-            {
-              forgotPassword ? (
-                <>
-                  <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
-                    Reset Password
-                  </h1>
-                  <p className="text-center text-sm pt-4 px-16">
-                    Send a login link to your account&apos;s email.
-                  </p>
-                  <Form type="forgotPassword" isModal={isModal} onCloseAction={onCloseAction} resetPasswordFunc={pull_ForgotPassword} />
-                  <p className="text-center text-sm pt-8 pb-8 px-16">
-                    <button className="hover:opacity-80 transition-opacity tap-highlight-transparent font-semibold text-sm" onClick={() => pull_ForgotPassword(true)} >
-                      Back to Login
-                    </button>
-                  </p>
-                </>
-              ) : (
-                <>
-                  <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
-                    Welcome Back
-                  </h1>
-                  <Form type="login" isModal={isModal} onCloseAction={onCloseAction} resetPasswordFunc={pull_ForgotPassword} />
-                  <p className="text-center text-sm pt-8 pb-8 px-16">
-                    Don&apos;t have an account?{" "}
-                    {isModal ? (
-                      <button className="hover:opacity-80 transition-opacity tap-highlight-transparent font-semibold text-sm" {...(isModal ? { onClick: () => setSelected("/signup") } : { href: "/signup" })} >
-                        Sign Up
+          {selected === "/login" && (
+            <div key="/login" title="Log In" {...(isModal ? {} : { href: "/login", as: Link })}>
+              <Image
+                alt="Prospector Minerals"
+                width={100}
+                height={100}
+                className="relative mx-auto h-12 w-auto dark:scale-120 dark:rounded-full dark:border dark:border-stone-400 my-5"
+                src="/PM-Favicon-New-Square.svg"
+              />
+              {
+                forgotPassword ? (
+                  <>
+                    <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
+                      Reset Password
+                    </h1>
+                    <p className="text-center text-sm pt-4 px-16">
+                      Send a login link to your account&apos;s email.
+                    </p>
+                    <Form type="forgotPassword" isModal={isModal} onCloseAction={onCloseAction} resetPasswordFunc={pull_ForgotPassword} />
+                    <p className="text-center text-sm pt-8 pb-8 px-16">
+                      <button className="hover:opacity-80 transition-opacity tap-highlight-transparent font-semibold text-sm" onClick={() => pull_ForgotPassword(true)} >
+                        Back to Login
                       </button>
-                    ) : (
-                      <Link href="/signup">
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
+                      Welcome Back
+                    </h1>
+                    <Form type="login" isModal={isModal} onCloseAction={onCloseAction} resetPasswordFunc={pull_ForgotPassword} />
+                    <p className="text-center text-sm pt-8 pb-8 px-16">
+                      Don&apos;t have an account?{" "}
+                      {isModal ? (
                         <button className="hover:opacity-80 transition-opacity tap-highlight-transparent font-semibold text-sm" {...(isModal ? { onClick: () => setSelected("/signup") } : { href: "/signup" })} >
                           Sign Up
                         </button>
-                      </Link>
-                    )}
-                    {" "}
-                    for free.
-                  </p>
-                  <></>
-                  <div className="flex flex-col space-y-4 px-4 mt-8 mb-8 sm:px-16">
-                    <Suspense
-                      fallback={
-                        <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
-                      }
-                    >
-                    </Suspense>
-                  </div>
-                </>
-              )
-            }
-          </div>
-          <div key="/signup" title="Sign Up" {...(isModal ? {} : { href: "/signup", as: Link })}>
-            <Image
-              alt="Platforms Starter Kit"
-              width={100}
-              height={100}
-              className="relative mx-auto h-12 w-auto dark:scale-120 dark:rounded-full dark:border dark:border-stone-400 my-5"
-              src="/PM-Favicon-New-Square.svg"
-            />
-            <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
-              Get Started
-            </h1>
-            <Form type="register" isModal={isModal} onCloseAction={onCloseAction} />
-            <p className="text-center text-sm pt-8 pb-8 px-16">
-              Already have an account?{" "}
-              {isModal ? (
-                <button className="hover:opacity-80 transition-opacity tap-highlight-transparent font-semibold text-sm" {...(isModal ? { onClick: () => setSelected("/login") } : { href: "/login" })}>
-                  Log In
-                </button>
-              ) : (
-                <Link href="/login">
+                      ) : (
+                        <Link href="/signup">
+                          <button className="hover:opacity-80 transition-opacity tap-highlight-transparent font-semibold text-sm" {...(isModal ? { onClick: () => setSelected("/signup") } : { href: "/signup" })} >
+                            Sign Up
+                          </button>
+                        </Link>
+                      )}
+                      {" "}
+                      for free.
+                    </p>
+                    <></>
+                    <div className="flex flex-col space-y-4 px-4 mt-8 mb-8 sm:px-16">
+                      <Suspense
+                        fallback={
+                          <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
+                        }
+                      >
+                      </Suspense>
+                    </div>
+                  </>
+                )
+              }
+            </div>)
+          }
+          {selected === "/signup" && (
+            <div key="/signup" title="Sign Up" {...(isModal ? {} : { href: "/signup", as: Link })}>
+              <Image
+                alt="Platforms Starter Kit"
+                width={100}
+                height={100}
+                className="relative mx-auto h-12 w-auto dark:scale-120 dark:rounded-full dark:border dark:border-stone-400 my-5"
+                src="/PM-Favicon-New-Square.svg"
+              />
+              <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
+                Get Started
+              </h1>
+              <Form type="register" isModal={isModal} onCloseAction={onCloseAction} />
+              <p className="text-center text-sm pt-8 pb-8 px-16">
+                Already have an account?{" "}
+                {isModal ? (
                   <button className="hover:opacity-80 transition-opacity tap-highlight-transparent font-semibold text-sm" {...(isModal ? { onClick: () => setSelected("/login") } : { href: "/login" })}>
                     Log In
                   </button>
-                </Link>
-              )}
-              {" "}
-              instead.
-            </p>
-            <></>
-            <div className="flex flex-col space-y-4 px-4 mt-8 mb-8 sm:px-16">
-              <Suspense
-                fallback={
-                  <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
-                }
-              >
-              </Suspense>
-            </div>
-          </div>
+                ) : (
+                  <Link href="/login">
+                    <button className="hover:opacity-80 transition-opacity tap-highlight-transparent font-semibold text-sm" {...(isModal ? { onClick: () => setSelected("/login") } : { href: "/login" })}>
+                      Log In
+                    </button>
+                  </Link>
+                )}
+                {" "}
+                instead.
+              </p>
+              <></>
+              <div className="flex flex-col space-y-4 px-4 mt-8 mb-8 sm:px-16">
+                <Suspense
+                  fallback={
+                    <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
+                  }
+                >
+                </Suspense>
+              </div>
+            </div>)
+          }
         </div>
       </div>
     </div >
