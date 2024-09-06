0a2b3d79a0cc547511832ba6e9d362d950929746
eamongordon <82300336+eamongordon@users.noreply.github.com>
2024-09-05 17:36:52 -0700

    feat: update dashboard layout with sidebar

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,14 +1,16 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
+import { ReactNode, Suspense } from "react";
+import Profile from "@/components/profile";
+import Nav from "@/components/nav";
 
-export default async function RootLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    const session = await auth();
-    if (session) {
-        return redirect(`/account/settings`);
-    }
-    return (children)
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Nav>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Profile />
+        </Suspense>
+      </Nav>
+      <div className="min-h-screen dark:bg-black sm:pl-60">{children}</div>
+    </div>
+  );
 }
---
+++ apps/web/components/profile.tsx
@@ -0,0 +1,36 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import LogoutButton from "./logout-button";
+
+export default async function Profile() {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex w-full items-center justify-between">
+      <Link
+        href="/settings"
+        className="flex w-full flex-1 items-center space-x-3 rounded-lg px-2 py-1.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
+      >
+        <Image
+          src={
+            session.user.image ??
+            `data:image/png;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAwADIDASIAAhEBAxEB/8QAHAAAAQQDAQAAAAAAAAAAAAAABwABBAYDBQgC/8QAJBAAAQMFAAIBBQAAAAAAAAAAAQACAwQFBhESITGhFCJRcZH/xAAYAQADAQEAAAAAAAAAAAAAAAABAwQCAP/EAB4RAAMBAQABBQAAAAAAAAAAAAABAgMRBBITFCEx/9oADAMBAAIRAxEAPwDlRZoYHSnQCemhdK8ABXvFsedUvbtiDfApdKeLXLzvkqLPTOi9hHd2HtFNvjzr8Kj5DjT43O5Yf4t5r1o6lwHKZbOttz4CdgrWuGjpBrjAMkkkgcXDF7aKiVuxtHDELMyKNhLUH8NqmRyM6IRzxu4ROgaAR6Uu1NFOSTLIaaMxc8hV+72GOoY48Bb9s7SPBCczs1okJMeRU/Q55pgQyvGOA8tZ8IW3S3Pp5XbC6hv8cEsLt6QWzGlia95aAqM9XX6T6ZpA04KSmuYOj+0k8SZLZWup3ggogY/lBhDQX/KFgJHpZ4al8Z8ErNQqDNNHQdFljXMG3r3U5U1rdh6BUF5ljGuinlvcrxropHx0N95hQuuX7aQH/KoV7vX1Jd93tV2atkkPlxUZzi72U6c1IurbJBm8nykoqSYYP//Z`
+          }
+          width={40}
+          height={40}
+          alt={session.user.name ?? "User avatar"}
+          className="h-6 w-6 rounded-full"
+        />
+        <span className="truncate text-sm font-medium">
+          {session.user.name}
+        </span>
+      </Link>
+      <LogoutButton />
+    </div>
+  );
+}
