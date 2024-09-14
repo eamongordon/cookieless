d913e382c1075e3d9fe00a78bb8cc33f0ab7c52f
Eamon Gordon <ekeokigordon@icloud.com>
2024-09-14 16:07:41 -0700

    feat: add next themes provider to web layout

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -4,13 +4,20 @@ import { ModalProvider } from "@/components/modal/provider";
 import { Analytics } from "@repo/next";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner"
+import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <Analytics siteId="6180cf4f-50fe-4409-97b6-bbe4a28496a6">
             <SessionProvider>
                 <ModalProvider>
-                    {children}
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                    >
+                        {children}
+                    </ThemeProvider>
                     <Toaster />
                 </ModalProvider>
             </SessionProvider>
