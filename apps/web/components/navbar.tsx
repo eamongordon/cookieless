c8e49b73b406b7908a94647e67dda4fa43168c4f
Eamon Gordon <ekeokigordon@icloud.com>
2024-09-15 15:44:19 -0700

    refactor: enhance navbar with mobile menu icons and update button styles

--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React, { useState } from 'react';
-import { buttonVariants } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 import Link from 'next/link';
+import { Menu, X } from 'lucide-react';
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -18,7 +19,7 @@ export default function Navbar() {
         {/* Menu items and button */}
         <div className="flex-1 flex items-center justify-end">
           <div className="hidden md:flex space-x-4">
-            <Link href="#home" className="text-neutral-700 hover:text-dough-500 px-3 py-2 text-sm font-semibold">Home</Link>
+            <Link href="#home" className=" px-3 py-2 text-sm font-semibold">Home</Link>
             <Link href="#features" className="text-neutral-700 hover:text-dough-500 px-3 py-2 text-sm font-semibold">Features</Link>
             <Link href="#contact" className="text-neutral-700 hover:text-dough-500 px-3 py-2 text-sm font-semibold">Contact</Link>
             <div className='ml-4 space-x-2'>
@@ -29,31 +30,27 @@ export default function Navbar() {
         </div>
         {/* Mobile menu button */}
         <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
-          <button
+          <Button
             type="button"
-            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            variant="ghost"
             aria-controls="mobile-menu"
             aria-expanded={isMenuOpen}
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             <span className="sr-only">Open main menu</span>
             {/* Icon when menu is closed. */}
-            <svg className={`${isMenuOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
-            {/* Icon when menu is open. */}
-            <svg className={`${isMenuOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
+              <Menu className={`${isMenuOpen ? 'hidden' : 'block'} h-6 w-6`} />
+              {/* Icon when menu is open. */}
+              <X className={`${isMenuOpen ? 'block' : 'hidden'} h-6 w-6`} />
+          </Button>
         </div>
       </div>
       {/* Mobile menu */}
-      <div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden`} id="mobile-menu">
+      <div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden absolute w-full`} id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1">
-          <a href="#home" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
-          <a href="#features" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Features</a>
-          <a href="#contact" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
+          <Link href="#home" className="text-neutral-700 hover:text-dough-500 block px-3 py-2 rounded-md text-base font-medium">Home</Link>
+          <Link href="#features" className="text-neutral-700 hover:text-dough-500 block px-3 py-2 rounded-md text-base font-medium">Features</Link>
+          <Link href="#contact" className="text-neutral-700 hover:text-dough-500 block px-3 py-2 rounded-md text-base font-medium">Contact</Link>
           <Link href="/login" className={buttonVariants({ variant: "secondary", rounded: "full", className: "w-full" })}>Log In</Link>
           <Link href="/signup" className={buttonVariants({ variant: "default", rounded: "full", className: "w-full" })}>Sign Up</Link>
         </div>
--- apps/web/components/sites/create-site.tsx
+++ apps/web/components/sites/create-site.tsx
@@ -15,7 +15,6 @@ export function CreateSiteButton() {
   return (
     <Button
       onClick={() => modal?.show(<CreateSiteModal />)}
-      className="rounded-lg border border-black bg-black px-4 py-1.5 text-sm font-medium text-white transition-all hover:bg-white hover:text-black active:bg-stone-100 dark:border-stone-700 dark:hover:border-stone-200 dark:hover:bg-black dark:hover:text-white dark:active:bg-stone-800"
     >
       Create New Site
     </Button>
