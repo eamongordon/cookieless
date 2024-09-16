f22d7e02b5f4992e0e521705b0adcf3e065a69c3
eamongordon <82300336+eamongordon@users.noreply.github.com>
2024-09-16 10:58:20 -0700

    refactor: move workSans font to utils file

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Work_Sans } from 'next/font/google'
+import { workSansClassName } from "@/lib/utils";
 import { Providers } from "./providers";
-
-const workSans = Work_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${workSans.className}`}>
+      <body className={`${workSansClassName}`}>
         <Providers>
           <div className="h-screen">
             {children}
--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -4,21 +4,15 @@ import { ModalProvider } from "@/components/modal/provider";
 import { Analytics } from "@repo/next";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner"
-import { ThemeProvider } from "next-themes";
+import { workSansClassName } from "@/lib/utils";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <Analytics siteId="8fffaf8b-2177-4f42-95ac-0ff9ce3e2f88">
+        <Analytics siteId="6180cf4f-50fe-4409-97b6-bbe4a28496a6">
             <SessionProvider>
                 <ModalProvider>
-                    <ThemeProvider
-                        attribute="class"
-                        defaultTheme="system"
-                        enableSystem
-                    >
-                        {children}
-                    </ThemeProvider>
-                    <Toaster />
+                    {children}
+                    <Toaster className={workSansClassName} />
                 </ModalProvider>
             </SessionProvider>
         </Analytics>
