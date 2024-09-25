f81ac682b241f8fd267f8a31ad2422d745499a4d
eamongordon <82300336+eamongordon@users.noreply.github.com>
2024-09-24 21:34:57 -0700

    fix: make aggregateEvents sql injection proof

--- packages/database/src/aggregations.ts
+++ packages/database/src/aggregations.ts
@@ -1,4 +1,4 @@
-import { eq, and, sql } from "drizzle-orm";
+import { sql, type SQL } from "drizzle-orm";
 import { db } from "./db";
 import { events } from "./schema";
 
@@ -92,48 +92,64 @@ export async function aggregateEvents({
     const validFields = ['name', 'timestamp', 'type', 'url', 'useragent', 'visitorHash', 'revenue']; // Add all valid fields here
 
     // Validate and sanitize fields
-    const sanitizedFields = aggregations.filter(field => validFields.includes(field.property as string) || isValidFieldName(field.property)).sort(); // Sort the fields to ensure consistent order
+    const sanitizedFields = aggregations.filter(field => validFields.includes(field.property as string)).sort(); // Sort the fields to ensure consistent order
     const deduplicatedFields = Array.from(new Set(sanitizedFields.map(field => field.property))); // Deduplicate the fields
-    // Construct the WHERE clause based on filters
 
     const isNotNestedFilter = (filter: Filter): filter is (PropertyFilter | CustomFilter) => {
         return !('nestedFilters' in filter);
     };
 
-    const buildFilters = (providedFilters: Filter[]): string => {
-        return providedFilters.map((filter, index) => {
+    const assignAliases = (fields: string[]) => {
+        return fields.reduce((acc, field, index) => {
+            acc[field] = `alias${index}`;
+            return acc;
+        }, {} as Record<string, string>);
+    };
+
+    const fieldAliases = assignAliases(deduplicatedFields);
+
+    const buildFilters = (providedFilters: Filter[]): SQL<unknown> => {
+        return sql.join(providedFilters.map((filter, index) => {
+            const filterLogical = index > 0 ? filter.logical ?? "AND" : "";
+            if (filterLogical !== "AND" && filterLogical !== "OR" && filterLogical !== "") {
+                throw new Error("Invalid logical operator");
+            }
             if (filter.nestedFilters && filter.nestedFilters.length > 0) {
                 const nestedConditions = buildFilters(filter.nestedFilters);
-                return `${index > 0 ? filter.logical ?? "AND" : ""} (${nestedConditions})`;
+                return sql`${sql.raw(filterLogical)} (${nestedConditions})`;
             } else if (isNotNestedFilter(filter)) {
                 const operator = getSqlOperator(filter.selector);
                 const value = filter.selector === 'contains' || filter.selector === 'doesNotContain' ? `%${filter.value}%` : filter.value;
                 if (filter.isCustom) {
-                    return `${index > 0 ? filter.logical ?? "AND" : ""} "customFields" ->> '${filter.property}' ${operator} '${filter.value}' `
+                    return sql`${sql.raw(filterLogical)} "customFields" ->> ${filter.property} ${sql.raw(operator)} ${value}`
                 } else {
-                    return `${index > 0 ? filter.logical ?? "AND" : ""} ${filter.property} ${operator} '${value}'`;
+                    return sql`${sql.raw(filterLogical)} ${events[filter.property]} ${sql.raw(operator)} ${value}`;
                 }
             } else {
                 throw new Error("Invalid filter configuration");
             }
-        }).join(` `);
+        }), sql` `);
     };
     
     // Generate the dynamic SQL for the fields
     const fieldQueries = sanitizedFields.map(field => {
+        const fieldAlias = fieldAliases[field.property];
+        if (!fieldAlias) {
+            throw new Error(`Invalid column name: ${field.property}`);
+        }
         return sql`
                 SELECT
                     interval,
                     ${sql`${field.property}`} AS field,
                     ${sql`${field.operator}`} AS operator,
-                    ${field.operator === "count" ? sql.identifier(field.property) : sql`NULL `} AS value,
+                    ${field.operator === "count" ? sql.identifier(fieldAlias) : sql`NULL `} AS value,
                     ${field.operator === "count"
                 ? field.countNull ? sql`COUNT(*) AS result`
-                    : sql`COUNT(CASE WHEN ${sql.identifier(field.property)} IS NOT NULL THEN 1 END) AS result`
-                : sql`${field.operator === "sum" ? sql`SUM` : sql`AVG`}(CAST(${sql.identifier(field.property)} AS NUMERIC)) AS result`}
+                    : sql`COUNT(CASE WHEN ${sql.identifier(fieldAlias)} IS NOT NULL THEN 1 END) AS result`
+                : sql`${field.operator === "sum" ? sql`SUM` : sql`AVG`}(CAST(${sql.identifier(fieldAlias)} AS NUMERIC)) AS result`}
                 FROM joined_intervals
-                ${field.countNull ? sql`` : sql`WHERE ${sql.identifier(field.property)} IS NOT NULL`}
-                GROUP BY interval${field.operator === "count" ? sql`, ${sql.identifier(field.property)}` : sql``}
+                ${field.countNull ? sql`` : sql`WHERE ${sql.identifier(fieldAlias)} IS NOT NULL`}
+                GROUP BY interval${field.operator === "count" ? sql`, ${sql.identifier(fieldAlias)}` : sql``}
             `;
     });
 
@@ -142,16 +158,19 @@ export async function aggregateEvents({
             SELECT
                 gs.interval,
                 ${sql.join(deduplicatedFields.map(field => {
+                    if (!fieldAliases[field]) {
+                        throw new Error(`Invalid column name: ${field}`);
+                    }
                     if (validFields.includes(field as string)) {
-                        return sql`${events[field as keyof typeof events]}`;
+                        return sql`${events[field as keyof typeof events]} AS ${sql.identifier(fieldAliases[field])}`;
                     } else {
-                        return sql`"customFields" ->> ${field} AS ${sql.identifier(field)}`;
+                        return sql`"customFields" ->> ${field} AS ${sql.identifier(fieldAliases[field])}`;
                     }
                 }), sql`, `)}
             FROM generate_series(0, ${intervals - 1}) AS gs(interval)
             LEFT JOIN ${events} ON ${events.timestamp} >= ${sql`${timeStart}::timestamp`} + interval '1 millisecond' * ${sql`${intervalDuration}`} * gs.interval
               AND ${events.timestamp} < ${sql`${timeStart}::timestamp`} + interval '1 millisecond' * ${sql`${intervalDuration}`} * (gs.interval + 1)
-              ${filters.length > 0 ? sql`WHERE ${sql.raw(buildFilters(filters))}` : sql``}
+              ${filters.length > 0 ? sql`WHERE ${buildFilters(filters)}` : sql``}
         )
         ${sql.join(fieldQueries, sql` UNION ALL `)}
     `);
@@ -201,9 +220,4 @@ function getSqlOperator(selector: Selectors): string {
         default:
             throw new Error(`Unknown selector: ${selector}`);
     }
-}
-
-function isValidFieldName(field: string): boolean {
-    // Add your custom field name validation logic here
-    return true;
 }
