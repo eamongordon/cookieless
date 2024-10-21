dd44bc94687e2bb78171c72e83d432eca754750f
eamongordon <82300336+eamongordon@users.noreply.github.com>
2024-10-21 16:47:54 -0700

    refactor: fix filter modal bg top gap issue

--- apps/web/app/(dashboard)/sites/[id]/page.tsx
+++ apps/web/app/(dashboard)/sites/[id]/page.tsx
@@ -4,7 +4,7 @@ export default function SitePage() {
     return (
         <div className="flex justify-center items-center">
             <div className="flex w-full max-w-screen-xl flex-col space-y-12 p-8">
-                <div className="flex flex-col space-y-6">
+                <div className="flex flex-col gap-6">
                     <h1 className="text-3xl font-semibold dark:text-white">
                         Analytics
                     </h1>
