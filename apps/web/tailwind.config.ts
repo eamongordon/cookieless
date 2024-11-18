b7b9cdfbe3964bf15de3aa9ca52b8cdb2a823692
eamongordon <82300336+eamongordon@users.noreply.github.com>
2024-11-18 09:18:26 -0800

    chore: add shadcn sidebar

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -7,23 +7,33 @@ module.exports = {
     "./lib/charts/chartUtils.ts"
   ],
   theme: {
-    extend: {
-      colors: {
-        'dough': {
-          '50': '#fcf9f0',
-          '100': '#f8f1dc',
-          '200': '#f0e0b8',
-          '300': '#e3c47f',
-          '400': '#dbac5c',
-          '500': '#d2953d',
-          '600': '#c47e32',
-          '700': '#a3632b',
-          '800': '#835029',
-          '900': '#6a4324',
-          '950': '#392111',
-        }
-      }
-    }
+  	extend: {
+  		colors: {
+  			dough: {
+  				'50': '#fcf9f0',
+  				'100': '#f8f1dc',
+  				'200': '#f0e0b8',
+  				'300': '#e3c47f',
+  				'400': '#dbac5c',
+  				'500': '#d2953d',
+  				'600': '#c47e32',
+  				'700': '#a3632b',
+  				'800': '#835029',
+  				'900': '#6a4324',
+  				'950': '#392111'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 }
