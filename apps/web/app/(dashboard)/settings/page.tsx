72825bd0ad7542c284eb89fbad7e0977bf8f2b56
Eamon Gordon <ekeokigordon@icloud.com>
2025-04-03 10:03:49 -0700

    feat: add breadcrumb navigation and sidebar trigger to personal account settings and sites pages

--- apps/web/app/(dashboard)/settings/page.tsx
+++ apps/web/app/(dashboard)/settings/page.tsx
@@ -2,6 +2,10 @@ import Form from "@/components/form";
 import { auth } from "@/lib/auth";
 import { editUserWrapper } from "@/lib/actions";
 import DeleteForm from "@/components/form/delete";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
 export default async function SettingsPage() {
     const session = await auth();
@@ -9,55 +13,76 @@ export default async function SettingsPage() {
         return null;
     }
     return (
-        <main>
-            <div className="flex justify-center items-center">
-                <div className="flex w-full max-w-screen-xl flex-col space-y-12 p-8">
-                    <div className="flex flex-col space-y-6">
-                        <h1 className="text-3xl font-semibold dark:text-white">
-                            Settings
-                        </h1>
-                        <Form
-                            type="user"
-                            title="Name"
-                            description="Your name."
-                            helpText="Please use 32 characters maximum."
-                            inputAttrs={{
-                                name: "name",
-                                type: "text",
-                                defaultValue: session.user.name!,
-                                placeholder: "John Doe",
-                                maxLength: 32,
-                            }}
-                        />
-                        <Form
-                            type="user"
-                            title="Email"
-                            description="Your email address used to login."
-                            helpText="Please enter a valid email."
-                            inputAttrs={{
-                                name: "email",
-                                type: "email",
-                                defaultValue: session.user.email!,
-                                placeholder: "email@example.com",
-                                required: true
-                            }}
-                        />
-                        <Form
-                            type="user"
-                            title="New Password"
-                            description="Create a new password"
-                            helpText="Please enter a valid password."
-                            inputAttrs={{
-                                name: "password",
-                                type: "password",
-                                defaultValue: "",
-                                required: true
-                            }}
-                        />
-                        <DeleteForm type="user" />
-                    </div>
+        <>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink asChild>
+                                    <Link href="/sites">
+                                        Personal Account
+                                    </Link>
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>
+                                    Settings
+                                </BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
                 </div>
-            </div>
-        </main>
+            </header>
+            <main className="flex flex-1 flex-col gap-4 p-8 pt-0">
+                <div className="max-w-screen-x flex flex-col space-y-6">
+                    <h1 className="text-3xl font-semibold dark:text-white">
+                        Settings
+                    </h1>
+                    <Form
+                        type="user"
+                        title="Name"
+                        description="Your name."
+                        helpText="Please use 32 characters maximum."
+                        inputAttrs={{
+                            name: "name",
+                            type: "text",
+                            defaultValue: session.user.name!,
+                            placeholder: "John Doe",
+                            maxLength: 32,
+                        }}
+                    />
+                    <Form
+                        type="user"
+                        title="Email"
+                        description="Your email address used to login."
+                        helpText="Please enter a valid email."
+                        inputAttrs={{
+                            name: "email",
+                            type: "email",
+                            defaultValue: session.user.email!,
+                            placeholder: "email@example.com",
+                            required: true
+                        }}
+                    />
+                    <Form
+                        type="user"
+                        title="New Password"
+                        description="Create a new password"
+                        helpText="Please enter a valid password."
+                        inputAttrs={{
+                            name: "password",
+                            type: "password",
+                            defaultValue: "",
+                            required: true
+                        }}
+                    />
+                    <DeleteForm type="user" />
+                </div>
+            </main>
+        </>
     );
 }
--- apps/web/app/(dashboard)/sites/page.tsx
+++ apps/web/app/(dashboard)/sites/page.tsx
@@ -2,29 +2,58 @@ import { Suspense } from "react";
 import Sites from "@/components/sites/sites";
 import { CreateSiteButton } from "@/components/modal/create-site";
 import { PlaceholderSiteCard } from "@/components/sites/site-card";
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
 export default function AllSites({ params }: { params: { id: string } }) {
     return (
-        <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
-            <div className="flex flex-col space-y-6">
-                <div className="flex items-center justify-between">
-                    <h1 className="font-cal text-3xl font-bold dark:text-white">
-                        All Sites
-                    </h1>
-                    <CreateSiteButton />
+        <>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink asChild>
+                                    <Link href="/sites">
+                                        Personal Account
+                                    </Link>
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>
+                                    Settings
+                                </BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
                 </div>
-                <Suspense
-                    fallback={
-                        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
-                            {Array.from({ length: 8 }).map((_, i) => (
-                                <PlaceholderSiteCard/>
-                            ))}
-                        </div>
-                    }
-                >
-                    <Sites />
-                </Suspense>
-            </div>
-        </div>
+            </header>
+            <main className="flex flex-1 flex-col gap-4 p-8 pt-0">
+                <div className="flex flex-col space-y-6">
+                    <div className="flex items-center justify-between">
+                        <h1 className="font-cal text-3xl font-bold dark:text-white">
+                            All Sites
+                        </h1>
+                        <CreateSiteButton />
+                    </div>
+                    <Suspense
+                        fallback={
+                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
+                                {Array.from({ length: 8 }).map((_, i) => (
+                                    <PlaceholderSiteCard />
+                                ))}
+                            </div>
+                        }
+                    >
+                        <Sites />
+                    </Suspense>
+                </div>
+            </main>
+        </>
     );
 }
