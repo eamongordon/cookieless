ec71c88f51c8ace29acd3151a1dd4e73d8e3bf91
Eamon Gordon <ekeokigordon@icloud.com>
2025-04-07 12:45:43 -0700

    refactor: remove unnecessary spacing class from sidebar header popover

--- apps/web/components/team-switcher.tsx
+++ apps/web/components/team-switcher.tsx
@@ -66,7 +66,7 @@ export function TeamSwitcher() {
             <PopoverTrigger asChild>
               <SidebarMenuButton
                 size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground space-x-2"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
               >
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold text-sidebar-accent-foreground">
@@ -74,7 +74,7 @@ export function TeamSwitcher() {
                   </span>
                   <span className="truncate text-xs">Hobby</span>
                 </div>
-                <ChevronsUpDown className="ml-auto" size={16} />
+                <ChevronsUpDown size={16} />
               </SidebarMenuButton>
             </PopoverTrigger>
             <PopoverContent
