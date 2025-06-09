830a27280481bb6e9ac979717d3ebaeef227463b
Eamon Gordon <ekeokigordon@icloud.com>
2025-06-08 18:04:28 -0700

    feat: add set default payment component to billing page

--- apps/web/components/billing/set-default-payment.tsx
+++ apps/web/components/billing/set-default-payment.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { updateDefaultPaymentMethod } from "@/lib/stripe/actions";
-import { CheckCircle, Loader2 } from "lucide-react";
-import { useState } from "react";
+import { AlertCircleIcon, CheckCircle, Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 
@@ -10,9 +10,17 @@ export function SetDefaultPayment() {
     const [status, setStatus] = useState<"success" | "failure" | "loading">("loading");
 
     const url = new URL(window.location.href);
+    const error = url.searchParams.get("error");
     const setupIntent = url.searchParams.get("setup_intent");
     const redirectStatus = url.searchParams.get("redirect_status");
     const paymentSuccess = url.searchParams.get("payment");
+
+    useEffect(() => {
+        if (error) {
+            setStatus("failure");
+        }
+    }, [error]);
+
     if (setupIntent && redirectStatus === "succeeded" && paymentSuccess === "success") {
         // Call backend action to set the new payment method as default
         (async () => {
@@ -29,6 +37,19 @@ export function SetDefaultPayment() {
                 <AlertTitle>Success!</AlertTitle>
                 <AlertDescription>
                     Your payment method has been updated successfully.
+                </AlertDescription>
+            </Alert>
+        ) : status === "failure" ? (
+            <Alert variant="destructive">
+                <AlertCircleIcon />
+                <AlertTitle>There was an error updating your payment information.</AlertTitle>
+                <AlertDescription>
+                    <p>Please verify your billing information and try again.</p>
+                    <ul className="list-inside list-disc text-sm">
+                        <li>Check your card details</li>
+                        <li>Ensure sufficient funds</li>
+                        <li>Verify billing address</li>
+                    </ul>
                 </AlertDescription>
             </Alert>
         ) : (
