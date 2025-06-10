5765ecdf8ee64837c646860592e667a2ad452026
Eamon Gordon <ekeokigordon@icloud.com>
2025-06-09 22:40:18 -0700

    Revert "test: comment out error check for STRIPE_SECRET in Stripe initialization"
    This reverts commit 8e84f9e1bf28b0c4775fdfc4cc55e3cb8a6ba63d.

--- apps/web/lib/stripe/index.ts
+++ apps/web/lib/stripe/index.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
 const STRIPE_SECRET = process.env.STRIPE_SECRET;
-//if (!STRIPE_SECRET) throw new Error("STRIPE_SECRET is not defined in environment variables.");
+if (!STRIPE_SECRET) throw new Error("STRIPE_SECRET is not defined in environment variables.");
 
-export const stripe = new Stripe(STRIPE_SECRET!);
+export const stripe = new Stripe(STRIPE_SECRET);
