82f684da57658a372844bee43d3afca4afba06dd
Eamon Gordon <ekeokigordon@icloud.com>
2025-06-13 15:08:04 -0700

    fix: add additional fields to auth client

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,10 +1,13 @@
 
  
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.BASE_URL! // Optional if the API base URL matches the frontend
-}) as ReturnType<typeof createAuthClient>
+    baseURL: process.env.BASE_URL!, // Optional if the API base URL matches the frontend
+    plugins: [inferAdditionalFields<typeof auth>()],
+});
  
 export const signIn: typeof authClient.signIn = authClient.signIn;
 export const signOut: typeof authClient.signOut = authClient.signOut;
