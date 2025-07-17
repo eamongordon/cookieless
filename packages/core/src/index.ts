dd6d08daacc024645335bff583d3a93d450b180c
Eamon Gordon <ekeokigordon@icloud.com>
2025-07-16 21:36:28 -0700

    fix: update collection url in tracking script

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 import type { eventData } from "@repo/database";
 
-const apiUrl = 'http://localhost:3001/collect';
+const apiUrl = 'https://cookieless-api-server-production.up.railway.app/collect';
 
 export function sendAnalyticsData(data: eventData) {
     return fetch(apiUrl, {
